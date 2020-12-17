e4b43ff7ca07da89e265b70c85730a7cf6463a14
Pranav Paharia <pranav.paharia@mediamonks.com>
2020-12-17 19:27:17 +0530

    Added Assets andPlayer Prefabs

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,18 +1,111 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+
+    public int UpForceMag = 150;
+
+    Rigidbody2D rb_Player;
+    TextMeshProUGUI UI_Tpro;
+    BoxCollider2D boxCol;
+    Ray GroundRay;
+    RaycastHit2D groundHit;
+    bool bJump = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb_Player = GetComponent<Rigidbody2D>() as Rigidbody2D;
+        UI_Tpro = GameObject.FindGameObjectWithTag("Respawn").GetComponent<TextMeshProUGUI>() as TextMeshProUGUI;
+        boxCol = this.gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
+
+        if(UI_Tpro == null)
+        {
+            Debug.Log("No text mesh");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyUp(KeyCode.Space) && (GetPlayerVelocity() <= 0.1 || GetPlayerVelocity() >= -0.1))
+        {
+            Debug.Log("Jump");
+            PlayerJump();
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                UI_Tpro.SetText("Tap Begins "); 
+            }
+
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                UI_Tpro.SetText("Tap Ends ");
+            }
+        }
+
+        //Debug.Log("Value of Velocity " + GetPlayerVelocity() );
+        if(GetPlayerVelocity() <= 0 && boxCol.isTrigger)
+        {
+            Debug.Log("Enable Collision");
+            ChangeIstriggerProperty(false);
+        }
+
+        if(GetPlayerVelocity() >= 0.1 && !boxCol.isTrigger)
+        {
+            Debug.Log("Disable Collision");
+            ChangeIstriggerProperty(true);
+        }
+
+        if(GetPlayerVelocity() == 0)
+        {
+
+        }
         
+    }
+
+    void CheckGroundDistance()
+    {
+        groundHit = Physics2D.Raycast(transform.position, Vector2.down, 10);
+        
+        
+
+        if(groundHit.collider != null)
+        {
+            Debug.Log("Collider Distance: " + groundHit.collider.name);
+        }
+    }
+
+    float GetPlayerVelocity()
+    {
+        float vel = rb_Player.velocity.y;
+        return vel;
+    }
+
+
+    void ChangeIstriggerProperty(bool bValue)
+    {
+        boxCol.isTrigger = bValue;
+    }
+
+    void PlayerJump()
+    {
+        rb_Player.AddForce(Vector2.up * UpForceMag);
+        bJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGroundDistance();
     }
 }
