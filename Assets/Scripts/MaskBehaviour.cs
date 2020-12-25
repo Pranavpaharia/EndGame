6618f47e1b7b0f22a16c3c7de0d89fb54cd9b126
Pranav Paharia <pranav.paharia@mediamonks.com>
2020-12-25 22:11:39 +0530

    Added Sounds and Mask Prefab

---
+++ Assets/Scripts/MaskBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    AudioSource audioSource;
+
+    void Start()   
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            audioSource.Play();
+            Invoke("DestroyMask", 0.7f);
+
+        }
+    }
+
+    void DestroyMask()
+    {
+        Destroy(gameObject);
+    }
+}
